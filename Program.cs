8612b07bead5c1a2d5d5a50bfb3ec78aab23c015
Thanapon <thanapon.wani@kmutt.ac.th>
2022-09-16 12:24:10 +0700

    quiz

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        int money = 1000000;
+        string kfc = "bkt";
+        string locate =  "soi 27";
+        
+    }
+}
+
+
---
+++ menu 1.cs
@@ -0,0 +1,19 @@
+class Personlist
+{
+    public string name;
+    public string last;
+    public long ID;
+
+    public Personlist (string name , string last , long ID)
+    {
+        this.name = name;
+        this.last = last;
+        this.ID = ID;
+    }
+
+    public void PrintUserInformation()
+    {
+        Console.WriteLine("1.tawee JaiDD 153-698-4561237");
+    }
+}
+
